976deaf387f87021b53e526319ac8f6ed6aab4c1
Ryan Torres <ryanmt@uci.edu>
2015-10-23 02:37:56 -0700

    Starting making some scripts for the grid system.

---
+++ Eternal/Assets/Code/Scripts/Grid.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+    Tile[,] tiles;
+    [SerializeField] int gridwidth;
+    [SerializeField] int gridheight;
+	// Use this for initialization
+	void Start()
+    {
+        tiles = new Tile[gridwidth,gridheight];
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	    
+	}
+}
---
+++ Eternal/Assets/Code/Scripts/Unit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour
+{
+    //Units will hold all of the stats of a character.
+
+	// Use this for initialization
+	void Start()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	    
+	}
+}
